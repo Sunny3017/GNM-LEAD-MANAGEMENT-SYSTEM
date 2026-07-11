f6751e29d3c1f60a35615b3c15212c4a447bd01f
Sunny3017 <sunnychaudhary3792@gmail.com>
2026-07-11 13:08:25 +0530

    Update CORS to allow specific origins only

--- backend/server.js
+++ backend/server.js
@@ -58,7 +58,22 @@ const io = socketIo(server, {
   }
 });
 
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:3000", 
+  "http://localhost:5173", 
+  "https://gnm-lead-management-system-1.onrender.com"
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
 app.use(express.json());
 
 const limiter = rateLimit({
